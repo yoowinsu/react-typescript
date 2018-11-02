a1bcf828efbe5cf205293a1e7ec3ea39d8b4606f
yoowinsu <yoowinsu@126.com>
2018-11-02 18:32:03 +0800

    'modify'

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -211,7 +211,7 @@ module.exports = {
                           require('postcss-flexbugs-fixes'),
                           autoprefixer({
                             browsers: [
-                              '>1%',
+                              '>0.1%',
                               'last 4 versions',
                               'Firefox ESR',
                               'not ie < 9', // React doesn't support IE8 anyway
